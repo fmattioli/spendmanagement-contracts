3a0d3f7923dc15350587d7670e0afb94d3f757ec
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-05-21 13:08:13 +0100

    FEAT: Changing contract

---
+++ src/SpendManagement.Contracts/V1/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace SpendManagement.Contracts.V1.Entities
+{
+    public class Category
+    {
+        public Category(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

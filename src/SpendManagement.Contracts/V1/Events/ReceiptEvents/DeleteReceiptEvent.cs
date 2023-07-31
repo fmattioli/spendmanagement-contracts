2e877ad79df08bb01da9ed17c33fc2483e2c2ef3
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-07-31 20:18:31 +0100

    FEAT: Adding new commands and events

---
+++ src/SpendManagement.Contracts/V1/Events/ReceiptEvents/DeleteReceiptEvent.cs
@@ -0,0 +1,18 @@
+﻿using SpendManagement.Contracts.V1.Entities;
+using SpendManagement.Contracts.V1.Interfaces;
+using System.Runtime.Serialization;
+
+namespace SpendManagement.Contracts.V1.Events.ReceiptEvents
+{
+    public class DeleteReceiptEvent : IEvent
+    {
+        [IgnoreDataMember]
+        public string RoutingKey => Category.Id.ToString();
+
+        [IgnoreDataMember]
+        public DateTime EventCreatedDate { get; set; }
+
+        [DataMember(Order = 1)]
+        public Category Category { get; set; } = null!;
+    }
+}

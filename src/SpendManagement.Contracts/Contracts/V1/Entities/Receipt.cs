07e2102861edcb86e3e1954e766d760f0ad0a821
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-01-07 07:50:36 -0300

    FEAT: Relating the CategoryId to the Receipt

--- src/SpendManagement.Contracts/Contracts/V1/Entities/Receipt.cs
+++ src/SpendManagement.Contracts/Contracts/V1/Entities/Receipt.cs
@@ -2,14 +2,16 @@
 {
     public class Receipt
     {
-        public Receipt(Guid id, string establishmentName, DateTime receiptDate)
+        public Receipt(Guid id, Guid categoryId, string establishmentName, DateTime receiptDate)
         {
             Id = id;
+            CategoryId = categoryId;
             EstablishmentName = establishmentName;
             ReceiptDate = receiptDate;
         }
 
         public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
         public string EstablishmentName { get; set; }
         public DateTime ReceiptDate { get; set; }
 

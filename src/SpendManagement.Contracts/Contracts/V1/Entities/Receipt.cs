89a9a9cea92e229bfe607b65036091c68d1dc5c2
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-01-14 10:12:51 -0300

    FIX: Solving constructor and updating packages

--- src/SpendManagement.Contracts/Contracts/V1/Entities/Receipt.cs
+++ src/SpendManagement.Contracts/Contracts/V1/Entities/Receipt.cs
@@ -1,13 +1,13 @@
 ﻿namespace SpendManagement.Contracts.V1.Entities
 {
-    public class Receipt(Guid id, Guid categoryId, string establishmentName, DateTime receiptDate)
+    public class Receipt(Guid id, Guid categoryId, string establishmentName, DateTime receiptDate, decimal discount)
     {
         public Guid Id { get; set; } = id;
         public Guid CategoryId { get; set; } = categoryId;
         public string EstablishmentName { get; set; } = establishmentName;
         public DateTime ReceiptDate { get; set; } = receiptDate;
         public decimal Total { get; set; }
-        public decimal Discount { get; set; }
+        public decimal Discount { get; set; } = discount;
 
         public bool Validate()
         {

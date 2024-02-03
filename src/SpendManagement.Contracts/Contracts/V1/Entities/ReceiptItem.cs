676fa842669706b017273bf4898a8a461bb558bb
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-02-03 17:54:18 -0300

    FEAT: Adding logic to calculate total price based on discounts

--- src/SpendManagement.Contracts/Contracts/V1/Entities/ReceiptItem.cs
+++ src/SpendManagement.Contracts/Contracts/V1/Entities/ReceiptItem.cs
@@ -1,13 +1,29 @@
 ﻿namespace SpendManagement.Contracts.V1.Entities
 {
-    public class ReceiptItem(Guid id, string itemName, short quantity, decimal itemPrice, string observation, decimal itemDiscount)
+    public class ReceiptItem
     {
-        public Guid Id { get; set; } = id;
-        public string ItemName { get; set; } = itemName;
-        public short Quantity { get; set; } = quantity;
-        public decimal ItemPrice { get; set; } = itemPrice;
-        public string Observation { get; set; } = observation;
-        public decimal TotalPrice { get { return ItemPrice * Quantity; } }
-        public decimal ItemDiscount { get; set; } = itemDiscount;
+        public ReceiptItem(Guid id, string itemName, short quantity, decimal itemPrice, string observation, decimal itemDiscount)
+        {
+            Id = id;
+            ItemName = itemName;
+            Quantity = quantity;
+            ItemPrice = itemPrice;
+            Observation = observation;
+            ItemDiscount = itemDiscount;
+            TotalPrice = CalculateTotalPrice();
+        }
+
+        public Guid Id { get; set; }
+        public string ItemName { get; set; }
+        public short Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string Observation { get; set; }
+        public decimal ItemDiscount { get; set; }
+        public decimal TotalPrice { get; private set; }
+
+        private decimal CalculateTotalPrice()
+        {
+            return (Quantity* ItemPrice) - ItemDiscount;
+        }
     }
 }
